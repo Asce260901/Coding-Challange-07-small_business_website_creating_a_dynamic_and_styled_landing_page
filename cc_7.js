e5a0d4159be59d1f9e76cf7359eca3a32b261ca0
Asce260901 <ajm55@usf.edu>
2026-03-06 20:26:56 -0500

    added fav icon and logo tried to fix the get quote btn

--- cc_7.js
+++ cc_7.js
@@ -5,7 +5,7 @@ const updateSection = document.getElementById("updatecta");
 const headlineInput = document.getElementById("headlineinput");
 const updateButton = document.getElementById("updatebtn");
 const ctaHeadline = document.getElementById("ctaheadline");
-const quoteButton = document.getElementById("quotebtn");
+const quoteButton = document.getElementById("ctabtn");
 
 const correctPassword = "JLR2026";
 
@@ -36,5 +36,6 @@ updateButton.addEventListener("click", function () {
 });
 
 quoteButton.addEventListener("click", function () {
-    window.open("mailto:jlrupholstery@gmail.com");
+    window.open("https://mail.google.com/mail/?view=cm&fs=1&to=jlrupholstery@gmail.com&su=Free%20Estimate%20Request&body=Hello%20JLR%20Upholstery,%0A%0AI%20would%20like%20to%20request%20a%20free%20estimate.",
+        "_blank");
 });
